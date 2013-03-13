e8432e5e6e39de4cd3d7d25e03ea2004642754ac
Klara Sarban <klara.sarban@sveaekonomi.se>
2013-03-13 09:47:41 +0100

    removed set ipAddress from create order response, is never returned

--- java/test/se/sveaekonomi/webpay/integration/webservice/payment/InvoicePaymentTest.java
+++ java/test/se/sveaekonomi/webpay/integration/webservice/payment/InvoicePaymentTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import se.sveaekonomi.webpay.integration.WebPay;
 import se.sveaekonomi.webpay.integration.config.SveaConfig;
 import se.sveaekonomi.webpay.integration.order.row.Item;
+import se.sveaekonomi.webpay.integration.response.webservice.CreateOrderResponse;
 import se.sveaekonomi.webpay.integration.util.constant.COUNTRYCODE;
 import se.sveaekonomi.webpay.integration.util.constant.CURRENCY;
 import se.sveaekonomi.webpay.integration.webservice.svea_soap.SveaCreateOrder;
@@ -52,6 +53,48 @@ public class InvoicePaymentTest {
         assertEquals("194609052222", request.request.CreateOrderInformation.CustomerIdentity.NationalIdNumber);
         assertEquals(COUNTRYCODE.SE, request.request.CreateOrderInformation.CustomerIdentity.CountryCode);
         assertEquals("Individual", request.request.CreateOrderInformation.CustomerIdentity.CustomerType);    
+    }
+    
+    @Test
+    public void testInvoiceDoRequestObjectForCustomerIdentityIndividualFromSE() throws Exception {
+    	CreateOrderResponse response = WebPay.createOrder()
+    		.addOrderRow(Item.orderRow()
+                .setArticleNumber("1")
+                .setQuantity(2)
+                .setAmountExVat(100.00)
+                .setDescription("Specification")
+                .setName("Prod")
+                .setUnit("st")
+                .setVatPercent(25)
+                .setDiscountPercent(0))
+                
+             .addOrderRow(Item.orderRow()
+                .setArticleNumber("1")
+                .setQuantity(2)
+                .setAmountExVat(100.00)
+                .setDescription("Specification")
+                .setName("Prod")
+                .setVatPercent(25)
+                .setDiscountPercent(0))
+                                
+             .addCustomerDetails(Item.individualCustomer()
+                 .setNationalIdNumber("194605092222")
+                 .setIpAddress("123.123.123"))                      
+             .setCountryCode(COUNTRYCODE.SE)
+             .setOrderDate("2012-12-12")                
+             .setClientOrderNumber("33")
+             .setCurrency(CURRENCY.SEK)
+             .useInvoicePayment()
+        //	returns an InvoicePayment object
+             	//.prepareRequest();
+             .doRequest();
+        
+    	assertEquals(response.isOrderAccepted(), true);
+        //CustomerIdentity            
+    	
+      /*  assertEquals("194609052222", request.request.CreateOrderInformation.CustomerIdentity.NationalIdNumber);
+        assertEquals(COUNTRYCODE.SE, request.request.CreateOrderInformation.CustomerIdentity.CountryCode);
+        assertEquals("Individual", request.request.CreateOrderInformation.CustomerIdentity.CustomerType);*/    
     }
         
     @Test
