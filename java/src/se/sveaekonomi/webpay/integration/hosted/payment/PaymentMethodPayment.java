2c506630c36bb40557b3f4caf4482f8e20edb4c2
Kristian Madsen <kristian.madsen@sveaekonomi.se>
2014-09-19 14:02:24 +0200

    setPaymentMethod(PAYMENTMETHOD.PAYMENTPLAN) uses the correct country paymentmethod

--- java/src/se/sveaekonomi/webpay/integration/hosted/payment/PaymentMethodPayment.java
+++ java/src/se/sveaekonomi/webpay/integration/hosted/payment/PaymentMethodPayment.java
@@ -7,11 +7,12 @@ import javax.xml.stream.XMLStreamWriter;
 
 import se.sveaekonomi.webpay.integration.order.create.CreateOrderBuilder;
 import se.sveaekonomi.webpay.integration.util.constant.INVOICETYPE;
+import se.sveaekonomi.webpay.integration.util.constant.PAYMENTPLANTYPE;
 import se.sveaekonomi.webpay.integration.util.constant.PAYMENTMETHOD;
 
 /**
  * Defines one payment method. Directs directly to method without going through PayPage.
- * @author klar-sar
+ * @author klar-sar, Kristian Grossman-Madsen
  */
 public class PaymentMethodPayment extends HostedPayment<PaymentMethodPayment> {
     
@@ -62,9 +63,31 @@ public class PaymentMethodPayment extends HostedPayment<PaymentMethodPayment> {
                         invoicetype = INVOICETYPE.INVOICE_SE;
                 }
                 writeSimpleElement(xmlw, "paymentmethod", invoicetype.getValue());
+        	} 
         	// elseif( paymentMethod == PAYMENTMETHOD.PAYMENTPLAN) {	TODO handle paymentplan per country }
-        	
-            
+        	else if(paymentMethod == PAYMENTMETHOD.PAYMENTPLAN) {
+                PAYMENTPLANTYPE paymentplantype;
+                switch (createOrderBuilder.getCountryCode()) {
+                    case DE:
+                    	paymentplantype = PAYMENTPLANTYPE.PAYMENTPLAN_DE;
+                        break;
+                    case DK:
+                    	paymentplantype = PAYMENTPLANTYPE.PAYMENTPLAN_DK;
+                        break;
+                    case FI:
+                    	paymentplantype = PAYMENTPLANTYPE.PAYMENTPLAN_FI;
+                        break;
+                    case NO:
+                    	paymentplantype = PAYMENTPLANTYPE.PAYMENTPLAN_NO;
+                        break;
+                    case NL:
+                    	paymentplantype = PAYMENTPLANTYPE.PAYMENTPLAN_NL;
+                        break;
+                    case SE:
+                    default:
+                    	paymentplantype = PAYMENTPLANTYPE.PAYMENTPLAN_SE;
+                }
+                writeSimpleElement(xmlw, "paymentmethod", paymentplantype.getValue());
         	} 
         	// no need to convert payment method
         	else {
