dec9e1f20bb8c0598cefd846ae2e3c0889a0a8d3
Kristian Madsen <kristian.madsen@sveaekonomi.se>
2014-09-22 16:36:23 +0200

    added integrationtest for WebPayAdmin entrypoints, with failing test for cancelOrder()

---
+++ java/integrationtest/se/sveaekonomi/webpay/integration/WebPayAdminTest.java
@@ -0,0 +1,64 @@
+package se.sveaekonomi.webpay.integration;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.w3c.dom.NodeList;
+
+import se.sveaekonomi.webpay.integration.config.SveaConfig;
+import se.sveaekonomi.webpay.integration.order.row.Item;
+import se.sveaekonomi.webpay.integration.response.webservice.CloseOrderResponse;
+import se.sveaekonomi.webpay.integration.response.webservice.CreateOrderResponse;
+import se.sveaekonomi.webpay.integration.util.test.TestingTool;
+import se.sveaekonomi.webpay.integration.webservice.helper.WebServiceXmlBuilder;
+import se.sveaekonomi.webpay.integration.webservice.svea_soap.SveaCreateOrder;
+import se.sveaekonomi.webpay.integration.webservice.svea_soap.SveaRequest;
+import se.sveaekonomi.webpay.integration.webservice.svea_soap.SveaSoapBuilder;
+
+public class WebPayAdminTest {
+
+	public CreateOrderResponse createTestOrder() {
+
+		SveaSoapBuilder soapBuilder = new SveaSoapBuilder();
+        
+        SveaRequest<SveaCreateOrder> request = WebPay.createOrder(SveaConfig.getDefaultConfig())
+                .addOrderRow(TestingTool.createExVatBasedOrderRow("1"))
+                .addCustomerDetails(Item.individualCustomer()
+                    .setNationalIdNumber(TestingTool.DefaultTestIndividualNationalIdNumber))
+                .setCountryCode(TestingTool.DefaultTestCountryCode)
+                .setClientOrderNumber(TestingTool.DefaultTestClientOrderNumber)
+                .setOrderDate(TestingTool.DefaultTestDate)
+                .setCurrency(TestingTool.DefaultTestCurrency)
+                .useInvoicePayment()
+                .prepareRequest();
+        
+        WebServiceXmlBuilder xmlBuilder = new WebServiceXmlBuilder();
+        
+        String xml = xmlBuilder.getCreateOrderEuXml(request.request);
+        String url = SveaConfig.getTestWebserviceUrl().toString();
+        String soapMessage = soapBuilder.makeSoapMessage("CreateOrderEu", xml);
+        NodeList soapResponse = soapBuilder.createOrderEuRequest(soapMessage, url);
+        CreateOrderResponse response = new CreateOrderResponse(soapResponse);
+        
+        return response;
+	}
+		
+    @Test
+    public void test_cancelOrder_cancelInvoiceOrder() {
+    	    	
+    	// create an order using defaults
+    	CreateOrderResponse order = createTestOrder();
+        assertTrue(order.isOrderAccepted());
+
+        // test WebPay::closeOrder
+        CloseOrderResponse closeResponse = WebPayAdmin.cancelOrder(SveaConfig.getDefaultConfig())
+//                .setOrderId(order.orderId)
+//                .setCountryCode(TestingTool.DefaultTestCountryCode)
+                .cancelInvoiceOrder()
+                	.doRequest();
+        
+        assertTrue(closeOrderResponse.isOrderAccepted());
+        //assertTrue(closeOrderResponse.isOrderAccepted());		// TODO change to AdminService\CancelOrderResponse
+        
+    }
+}
