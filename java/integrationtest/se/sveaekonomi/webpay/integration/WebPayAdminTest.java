fdcbdca80bc55924969701fa5fe1ed420698a29a
Kristian Madsen <kristian.madsen@sveaekonomi.se>
2014-09-23 12:45:16 +0200

    created skeleton for test_cancelOrder_cancelCardOrder

--- java/integrationtest/se/sveaekonomi/webpay/integration/WebPayAdminTest.java
+++ java/integrationtest/se/sveaekonomi/webpay/integration/WebPayAdminTest.java
@@ -2,12 +2,21 @@ package se.sveaekonomi.webpay.integration;
 
 import static org.junit.Assert.*;
 
+import java.util.Date;
+
 import org.junit.Test;
 
 import se.sveaekonomi.webpay.integration.config.SveaConfig;
+import se.sveaekonomi.webpay.integration.hosted.helper.PaymentForm;
+import se.sveaekonomi.webpay.integration.order.create.CreateOrderBuilder;
+import se.sveaekonomi.webpay.integration.order.row.Item;
 import se.sveaekonomi.webpay.integration.response.webservice.CloseOrderResponse;
 import se.sveaekonomi.webpay.integration.response.webservice.CreateOrderResponse;
+import se.sveaekonomi.webpay.integration.util.constant.COUNTRYCODE;
+import se.sveaekonomi.webpay.integration.util.constant.PAYMENTMETHOD;
 import se.sveaekonomi.webpay.integration.util.test.TestingTool;
+import se.sveaekonomi.webpay.integration.webservice.svea_soap.SveaCreateOrder;
+import se.sveaekonomi.webpay.integration.webservice.svea_soap.SveaRequest;
 
 public class WebPayAdminTest {
 	
@@ -43,5 +52,29 @@ public class WebPayAdminTest {
                 	.doRequest();
         
         assertTrue(response.isOrderAccepted());        
+    }       
+    
+    @Test
+    public void test_cancelOrder_cancelCardOrder() {
+    	    	
+		// create order
+        CreateOrderBuilder order = WebPay.createOrder(SveaConfig.getDefaultConfig())
+        		// add order rows
+                .addOrderRow(TestingTool.createExVatBasedOrderRow("1"))
+                .addCustomerDetails(TestingTool.createIndividualCustomer(COUNTRYCODE.SE))
+                .setCountryCode(TestingTool.DefaultTestCountryCode)
+                //.setOrderDate(TestingTool.DefaultTestDate)
+                .setClientOrderNumber(Long.toString((new Date()).getTime()))
+                .setCurrency(TestingTool.DefaultTestCurrency)
+        ;
+                
+        // choose payment method and do request
+        PaymentForm form = order.usePaymentMethod(PAYMENTMETHOD.KORTCERT)
+                	.setReturnUrl("https://test.sveaekonomi.se/webpay-admin/admin/merchantresponsetest.xhtml")
+                	.getPaymentForm();
+
+        // TODO post order form and pick up transaction id from callbackurl
+        
+        // TODO do cancelCardOrder request and assert the response
     }       
 }
