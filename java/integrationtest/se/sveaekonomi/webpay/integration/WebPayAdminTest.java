646ef0f1981952a5d8e76b8b3ba4a347574f6b06
Kristian Madsen <kristian.madsen@sveaekonomi.se>
2014-09-23 17:18:23 +0200

    enters card order credentials at certitrade using selenium webdriver

--- java/integrationtest/se/sveaekonomi/webpay/integration/WebPayAdminTest.java
+++ java/integrationtest/se/sveaekonomi/webpay/integration/WebPayAdminTest.java
@@ -9,14 +9,19 @@ import org.junit.Test;
 import se.sveaekonomi.webpay.integration.config.SveaConfig;
 import se.sveaekonomi.webpay.integration.hosted.helper.PaymentForm;
 import se.sveaekonomi.webpay.integration.order.create.CreateOrderBuilder;
-import se.sveaekonomi.webpay.integration.order.row.Item;
 import se.sveaekonomi.webpay.integration.response.webservice.CloseOrderResponse;
 import se.sveaekonomi.webpay.integration.response.webservice.CreateOrderResponse;
 import se.sveaekonomi.webpay.integration.util.constant.COUNTRYCODE;
 import se.sveaekonomi.webpay.integration.util.constant.PAYMENTMETHOD;
 import se.sveaekonomi.webpay.integration.util.test.TestingTool;
-import se.sveaekonomi.webpay.integration.webservice.svea_soap.SveaCreateOrder;
-import se.sveaekonomi.webpay.integration.webservice.svea_soap.SveaRequest;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WebPayAdminTest {
 	
@@ -59,21 +64,49 @@ public class WebPayAdminTest {
     	    	
 		// create order
         CreateOrderBuilder order = WebPay.createOrder(SveaConfig.getDefaultConfig())
-        		// add order rows
                 .addOrderRow(TestingTool.createExVatBasedOrderRow("1"))
                 .addCustomerDetails(TestingTool.createIndividualCustomer(COUNTRYCODE.SE))
                 .setCountryCode(TestingTool.DefaultTestCountryCode)
                 //.setOrderDate(TestingTool.DefaultTestDate)
-                .setClientOrderNumber(Long.toString((new Date()).getTime()))
+                .setClientOrderNumber("test_cancelOrder_cancelCardOrder" + Long.toString((new Date()).getTime()))
                 .setCurrency(TestingTool.DefaultTestCurrency)
         ;
                 
         // choose payment method and do request
         PaymentForm form = order.usePaymentMethod(PAYMENTMETHOD.KORTCERT)
-                	.setReturnUrl("https://test.sveaekonomi.se/webpay-admin/admin/merchantresponsetest.xhtml")
-                	.getPaymentForm();
+                	.setReturnUrl("https://test.sveaekonomi.se/webpay-admin/admin/merchantresponsetest.xhtml")	// TODO change to localhost
+                	.getPaymentForm()
+    	;
 
         // TODO post order form and pick up transaction id from callbackurl
+        
+        // insert form in empty page
+        FirefoxDriver driver = new FirefoxDriver();
+        driver.get("about:blank");
+        String script = "document.body.innerHTML = '" + form.getCompleteForm() + "'";
+        driver.executeScript(script);
+        
+        // post form to certitrade page
+        WebElement element = driver.findElementById("paymentForm");
+        element.submit();
+
+        // enter certitrade card test credentials
+        WebElement cardno = driver.findElementById("cardno");
+        cardno.sendKeys("4444333322221100");       
+
+        WebElement cvc = driver.findElementById("cvc"); 	       
+    	cvc.sendKeys("123");
+
+        Select month = new Select(driver.findElementById("month"));
+        month.selectByValue("01");
+
+        Select year = new Select(driver.findElementById("year"));
+        year.selectByValue("17");
+        
+        // submit card order and handle callback
+        driver.findElementById("perform-payment").click();        
+
+        // TODO set up localhost return url and parse out transaction id
         
         // TODO do cancelCardOrder request and assert the response
     }       
