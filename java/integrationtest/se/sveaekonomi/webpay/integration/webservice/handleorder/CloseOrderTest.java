f2d2b109cb1e2d5f2a8f657ae70f4b2c470fa566
Kristian Madsen <kristian.madsen@sveaekonomi.se>
2014-09-29 17:23:40 +0200

    referal and dummy test added

--- java/integrationtest/se/sveaekonomi/webpay/integration/webservice/handleorder/CloseOrderTest.java
+++ java/integrationtest/se/sveaekonomi/webpay/integration/webservice/handleorder/CloseOrderTest.java
@@ -1,7 +1,12 @@
 package se.sveaekonomi.webpay.integration.webservice.handleorder;
 
+import org.junit.Test;
+
 public class CloseOrderTest {
    
 	// see WebPayTest.java test_closeOrder
-   
+    @Test	
+	public void test_foo() {
+		assert( true );
+	};
 }
