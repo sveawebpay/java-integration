8fe5dd1305619cb6fbcc2ce3ae03895c9fb7d1e6
Kristian Madsen <kristian.madsen@sveaekonomi.se>
2014-10-01 14:09:02 +0200

    finished CancelPaymentPlanOrder validation tests

--- java/test/se/sveaekonomi/webpay/integration/WebPayAdminUnitTestValidation.java
+++ java/test/se/sveaekonomi/webpay/integration/WebPayAdminUnitTestValidation.java
@@ -14,6 +14,10 @@ import se.sveaekonomi.webpay.integration.webservice.svea_soap.SveaRequest;
 
 public class WebPayAdminUnitTestValidation {    		
 	
+	
+	// validate required methods for invoice orders below:
+
+	
 	@Test
     public void test_validates_all_required_methods_for_cancelOrder_cancelInvoiceOrder_() {
     	    	
@@ -81,4 +85,77 @@ public class WebPayAdminUnitTestValidation {
     		);	
 		}
     }		
+	
+	
+	// validate required methods for payment plan orders below:
+
+	@Test
+    public void test_validates_all_required_methods_for_cancelOrder_cancelPaymentPlanOrder_() {
+    	    	
+        CancelOrderBuilder cancelOrderBuilder = WebPayAdmin.cancelOrder(SveaConfig.getDefaultConfig())
+                .setOrderId(TestingTool.DefaultOrderId)
+                .setCountryCode(TestingTool.DefaultTestCountryCode)
+        ;
+        
+		// prepareRequest() validates the order and throws SveaWebPayException on validation failure
+		try {
+			CloseOrder closeOrder = cancelOrderBuilder.cancelPaymentPlanOrder();            
+			SveaRequest<SveaCloseOrder> sveaRequest = closeOrder.prepareRequest();					
+		}
+		catch (SveaWebPayException e){
+			
+			// fail on validation error
+	        fail();
+        }
+    }
+	
+	@Test
+    public void test_missing_required_method_for_cancelOrder_cancelPaymentPlanOrder_setOrderId() {
+    	    	
+        CancelOrderBuilder cancelOrderBuilder = WebPayAdmin.cancelOrder(SveaConfig.getDefaultConfig())
+                //.setOrderId(TestingTool.DefaultOrderId)
+                .setCountryCode(TestingTool.DefaultTestCountryCode)
+        ;
+        
+		// prepareRequest() validates the order and throws SveaWebPayException on validation failure
+		try {
+			CloseOrder closeOrder = cancelOrderBuilder.cancelPaymentPlanOrder();
+			SveaRequest<SveaCloseOrder> sveaRequest = closeOrder.prepareRequest();
+
+			// fail if validation passes
+	        fail();
+        }
+		catch (SveaWebPayException e){
+	        assertEquals(
+        		"MISSING VALUE - OrderId is required, use setOrderId().\n", 
+    			e.getCause().getMessage()
+    		);	
+		}
+    }	    		
+
+	@Test
+    public void test_missing_required_method_for_cancelOrder_cancelPaymentPlanOrder_setCountryCode() {
+    	    	
+        CancelOrderBuilder cancelOrderBuilder = WebPayAdmin.cancelOrder(SveaConfig.getDefaultConfig())
+                .setOrderId(TestingTool.DefaultOrderId)
+                //.setCountryCode(TestingTool.DefaultTestCountryCode)
+        ;
+        
+		// prepareRequest() validates the order and throws SveaWebPayException on validation failure
+		try {
+			CloseOrder closeOrder = cancelOrderBuilder.cancelPaymentPlanOrder();
+			SveaRequest<SveaCloseOrder> sveaRequest = closeOrder.prepareRequest();
+
+			// fail if validation passes
+	        fail();
+        }
+		catch (SveaWebPayException e){
+	        assertEquals(
+        		"MISSING VALUE - CountryCode is required, use setCountryCode(...).\n", 
+    			e.getCause().getMessage()
+    		);	
+		}
+    }		
+	
+	
 }
