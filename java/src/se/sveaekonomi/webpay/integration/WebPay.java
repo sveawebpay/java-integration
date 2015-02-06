7bb3151a6d4880d6b5507d6fa60f076c2d3fe805
Kristian Madsen <kristian.madsen@sveaekonomi.se>
2015-02-06 15:40:12 +0100

    updated documentation for WebPay::createOrder

--- java/src/se/sveaekonomi/webpay/integration/WebPay.java
+++ java/src/se/sveaekonomi/webpay/integration/WebPay.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import se.sveaekonomi.webpay.integration.config.ConfigurationProvider;
 import se.sveaekonomi.webpay.integration.exception.SveaWebPayException;
+import se.sveaekonomi.webpay.integration.hosted.payment.PayPagePayment;
 import se.sveaekonomi.webpay.integration.order.create.CreateOrderBuilder;
 import se.sveaekonomi.webpay.integration.order.handle.CloseOrderBuilder;
 import se.sveaekonomi.webpay.integration.order.handle.DeliverOrderBuilder;
@@ -15,24 +16,92 @@ import se.sveaekonomi.webpay.integration.webservice.getpaymentplanparams.Payment
 
 /**
  * Start build request object by choosing the right method.
- * @author klar-sar
+ * @author klar-sar, Kristian Grossman-Madsen
  */
 public class WebPay {
     
     /**
-     * Start build order request to create an order for all payments.
-     * @param config
-     * @return CreateOrderBuilder
+     * Use WebPay.createOrder() to create an order using invoice, payment plan, card, or direct bank payment methods. 
+     * You may also send the customer to the PayPage, where they may select from all available payment methods.
+     *
+     * See the CreateOrderBuilder class for more info on methods used to specify the order builder contents, including
+     * order rows items et al, and then specifying which payment method to use, followed by sending the request to Svea
+     * using doRequest, and parsing the response received from Svea.
+     *
+     * Invoice and payment plan orders will perform a synchronous payment request, and will return a response 
+     * object immediately following the doRequest call.
+     * 
+     * Hosted payment methods like Card, Direct Bank and any payment methods accessed via the PayPage, are asynchronous.
+     * Having selecting an asynchronous payment method you generally use a request class method to get a payment form 
+     * object in return. The form is then posted to Svea, where the customer is redirected to the card payment provider 
+     * service or bank. After the customer completes the payment, a response is sent back to your provided return url, 
+     * where it can be processed and inspected.
+     * 
+     * Card, Direct Bank, and other hosted methods accessed via PayPage are asynchronous. Asynchronous payment methods
+     * provide an html form containing a formatted message to send to Svea, which in turn will send a request response 
+     * to the specified return url, where the response can be parsed using the SveaResponse class. You should also be
+     * prepared to receive the request response on the specified alternative callback url which is used, amongst others,
+     * if i.e. the customer does not return to the store after the order payment have been completed.
+     * 
+     * To create an invoice or partpayment order using useInvoicePayment or usePaymentPlanPayment, you do not need to 
+     * explicitly specify which payment methods are available. 
+     * 
+     * When creating a card or direct bank order, you can minimize the number of steps in the checkout process by 
+     * explicitly specifying i.e. usePaymentMethod(PAYMENTMETHOD.KORTCERT) instead of going through useCardPayment.
+     * 
+     * Get an order builder instance using the WebPay.deliverOrder entrypoint, then provide more information about the 
+     * transaction using DeliverOrderBuilder methods: 
+     * 
+     * When redirecting the customer to the PayPage, you can use methods in PayPagePayment, i.e. excludePaymentMethods, 
+     * to first specify which available payment methods to show or exclude, followed by the doRequest call.
+     * 
+     * ...
+     *      orderbuilder = WebPay.createOrder(config)
+     *       	.addOrderRow()              		// required, see WebPayItem.orderRow() for order row specification
+     *          .addFee()         			   		// optional, see WebPayItem for invoice, shipping fee
+     *          .addDiscount()          			// optional, see WebPayItem for fixed, relative discount
+     *          .addCustomerDetails()    			// required for invoice and payment plan payments, see WebPayItem for individual, company customer
+     *          .setCountryCode()               	// required
+     *          .setOrderDate()            			// required for invoice and payment plan payments
+     *          .setCurrency()                 		// required for card payment, direct bank & PayPage payments. Ignored for invoice and payment plan.
+     *          .setClientOrderNumber()    			// required for card payment, direct payment, PaymentMethod & PayPage payments, max length 30 chars.
+     *          .setCustomerReference()    			// optional, ignored for card & direct bank orders, max length 30 chars.
+     *      ;
+     *      
+     *      // then select a synchronous payment method (invoice, part payment) request class and send request
+     *      response = orderbuilder.useInvoicePayment().doRequest();    	// returns CreateOrderResponse
+     *      response = orderbuilder.usePaymentPlanPayment().doRequest();	// returns CreateOrderResponse
+     *      
+     *      // or select an asynchronous payment method (card, direct bank et al.) request class
+     *      request = orderbuilder
+     *      	.usePaymentMethod(PAYMENTMETHOD.KORTCERT)	// returns HostedPayment<?>
+	 *			.usePayPage()								// returns PayPagePayment
+     *      	.usePayPageCardOnly()						// returns CardPayment
+     *      	.usePayPageDirectBankOnly()					// returns DirectPayment
+     *      ;
+     *      // then perform any additional request settings needed and receive request information
+     *      request.
+	 *			.setReturnUrl()				// required
+     *      	.setCallbackUrl()			// optional but recommended
+     *      	.setCancelUrl()				// optional, paypage only
+     *      	.setPayPageLanguageCode()	// optional, defaults to english
+     *      	.setSubscriptionType()		// optional, card only, used to setup recurring payments
+     *      	.setSubscriptionId()		// required for card doRecur request
+     *      ;
+     *      form = request.getPaymentForm();	// returns PaymentForm object containing request html form
+     *      url = request.getPaymentUrl();		// returns PaymentUrl object containing url to prepared payment request
+     *      response = request.doRecur();		// performs synchronous request, returns RecurTransactionResponse
+     * ...
+     * 
      */
-    public static CreateOrderBuilder createOrder(ConfigurationProvider config) {
+	public static CreateOrderBuilder createOrder(ConfigurationProvider config) {
         if (config == null) {
             throw new SveaWebPayException("A configuration must be provided. For testing purposes use SveaConfig.GetDefaultConfig()");
         }
     	
         return new CreateOrderBuilder(config);
     }  
-     
-    
+
     
     /**
      * Use the WebPay.deliverOrder() entrypoint when you deliver an order to the customer. 
