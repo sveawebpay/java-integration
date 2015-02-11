649ed1f4d3c7cc574f3502103e6c8b75e5af9c5c
Kristian Madsen <kristian.madsen@sveaekonomi.se>
2015-02-11 10:41:51 +0100

    validation unit tests passing

--- java/src/se/sveaekonomi/webpay/integration/adminservice/DeliverOrdersRequest.java
+++ java/src/se/sveaekonomi/webpay/integration/adminservice/DeliverOrdersRequest.java
@@ -1,7 +1,10 @@
 package se.sveaekonomi.webpay.integration.adminservice;
 
+import javax.xml.bind.ValidationException;
+
 import se.sveaekonomi.webpay.integration.Requestable;
 import se.sveaekonomi.webpay.integration.order.handle.DeliverOrderBuilder;
+import se.sveaekonomi.webpay.integration.order.handle.QueryOrderBuilder;
 
 /**
  * DeliverOrdersRequest handles requests to Svea Admin WebService DeliverOrders method
@@ -10,11 +13,35 @@ import se.sveaekonomi.webpay.integration.order.handle.DeliverOrderBuilder;
  */
 public class DeliverOrdersRequest implements Requestable {
 	
-	private DeliverOrderBuilder order;
+	private String action;
+	public DeliverOrderBuilder builder;
 
     public DeliverOrdersRequest(DeliverOrderBuilder orderBuilder) {
-        this.order =  orderBuilder;
+		this.action = "DeliverOrders";
+		this.builder = orderBuilder;
     }
+    
+	/**
+	 * validates that all required attributes needed for the request are present in the builder object
+	 * @throws ValidationException
+	 */
+	public void validateOrder() throws ValidationException {
+		String errors = "";		
+        if (builder.getOrderId() == null) {
+            errors += "MISSING VALUE - OrderId is required, use setOrderId().\n";
+    	}   
+        
+        if (builder.getInvoiceDistributionType() == null) {
+            errors += "MISSING VALUE - distributionType is required, use setInvoiceDistributionType().\n";
+    	}   
+        
+        if (builder.getCountryCode() == null) {
+            errors += "MISSING VALUE - CountryCode is required, use setCountryCode().\n";
+        }     		
+        if ( !errors.equals("")) {
+            throw new ValidationException(errors);
+        }
+	}
 
     // TODO
 	public String prepareRequest() {
--- java/test/se/sveaekonomi/webpay/integration/adminservice/DeliverOrdersRequestTest.java
+++ java/test/se/sveaekonomi/webpay/integration/adminservice/DeliverOrdersRequestTest.java
@@ -42,76 +42,34 @@ public class DeliverOrdersRequestTest {
 	        fail();
         }		  
     }
-    
-//    @Test
-//    public void test_validates_all_required_methods_for_queryOrder_queryInvoiceOrder_using_prepareRequest() {
-//		QueryOrderBuilder builder = WebPayAdmin.queryOrder(SveaConfig.getDefaultConfig())
-//			.setOrderId(123456L)													// invoice, partpayment only, required
-//			.setCountryCode(TestingTool.DefaultTestCountryCode)						// required
-//		;			
-//		try {
-//			GetOrdersRequest request = builder.queryInvoiceOrder();						
-//			@SuppressWarnings("unused")
-//			SOAPMessage sm = request.prepareRequest();
-//		}
-//		catch (SveaWebPayException e){			
-//			// fail on validation error
-//	        fail();
-//		} 
-//		catch (SOAPException e) {
-//			// thrown by prepareRequest
-//			e.printStackTrace();
-//		}					
-//    }   
-//
-//    @Test
-//    public void test_validates_missing_methods_for_queryOrder_queryInvoiceOrder_using_validateOrder() {
-//		QueryOrderBuilder builder = WebPayAdmin.queryOrder(SveaConfig.getDefaultConfig())
-//			//.setOrderId(123456L)													// invoice, partpayment only, required
-//			//.setCountryCode(TestingTool.DefaultTestCountryCode)						// required
-//		;			
-//		try {			
-//			GetOrdersRequest request = builder.queryInvoiceOrder();						
-//			request.validateOrder();					
-//			// fail if validation passes
-//	        fail( "Expected ValidationException missing." );
-//        }
-//		catch (ValidationException e){		
-//			assertEquals( 
-//        		"MISSING VALUE - OrderId is required, use setOrderId().\n" +
-//        		"MISSING VALUE - CountryCode is required, use setCountryCode().\n",
-//    			e.getMessage()
-//    		);
-//        }		  
-//    }
-//
-//    @Test
-//    public void test_validates__missing_methods_for_queryOrder_queryInvoiceOrder_using_prepareRequest() {
-//		QueryOrderBuilder builder = WebPayAdmin.queryOrder(SveaConfig.getDefaultConfig())
-//			//.setOrderId(123456L)													// invoice, partpayment only, required
-//			//.setCountryCode(TestingTool.DefaultTestCountryCode)					// required
-//		;			
-//		try {			
-//			GetOrdersRequest request = builder.queryInvoiceOrder();						
-//			@SuppressWarnings("unused")
-//			SOAPMessage sm = request.prepareRequest();
-//			// fail if validation passes
-//	        fail( "Expected ValidationException missing." );
-//        }
-//		catch (SveaWebPayException e){		
-//			assertEquals( 
-//        		"MISSING VALUE - OrderId is required, use setOrderId().\n" +
-//        		"MISSING VALUE - CountryCode is required, use setCountryCode().\n",
-//    			e.getCause().getMessage()	// use getCause(), as SveaWebPayException wraps the ValidationException	
-//    		);
-//        }
-//		catch (SOAPException e) {
-//			// thrown by prepareRequest
-//			e.printStackTrace();
-//		}
-//    }    
-//   
-//  	// paymentplan
-//	// TODO public void test_validates_all_required_methods_for_queryOrder_queryPaymentPlanOrder() {
+     
+    @Test
+    public void test_deliverOrder_deliverInvoiceOrder_without_orderrows_return_DeliverOrdersResponse_validates_missing_methods() {
+		DeliverOrderBuilder builder = WebPay.deliverOrder(SveaConfig.getDefaultConfig())
+			//.addOrderRow()
+			//.setCountryCode(TestingTool.DefaultTestCountryCode)
+			//.setOrderId( 123456L )
+			//.setInvoiceDistributionType( DISTRIBUTIONTYPE.Post )
+		;
+		DeliverOrdersRequest request = builder.deliverInvoiceOrder();
+		assertThat( request, instanceOf(DeliverOrdersRequest.class) );
+				
+		try {			
+			request.validateOrder();					
+			// fail if validation passes
+	        fail( "Expected ValidationException missing." );
+        }
+		catch (ValidationException e){		
+			assertEquals( 
+        		"MISSING VALUE - OrderId is required, use setOrderId().\n" +
+				"MISSING VALUE - distributionType is required, use setInvoiceDistributionType().\n" +
+        		"MISSING VALUE - CountryCode is required, use setCountryCode().\n",
+    			e.getMessage()
+    		);
+        }		  
+    }
+
+  	// paymentplan
+	// TODO public void test_validates_all_required_methods_for_queryOrder_queryPaymentPlanOrder() {
 
 }
