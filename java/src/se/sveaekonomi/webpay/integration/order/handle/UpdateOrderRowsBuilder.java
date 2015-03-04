2e14c10de1ae1623ad51c4c12c8e2ab7fa4d73c5
Kristian Madsen <kristian.madsen@sveaekonomi.se>
2015-03-04 13:57:44 +0100

    UpdateOrderRowsBuilder types

--- java/src/se/sveaekonomi/webpay/integration/order/handle/UpdateOrderRowsBuilder.java
+++ java/src/se/sveaekonomi/webpay/integration/order/handle/UpdateOrderRowsBuilder.java
@@ -6,68 +6,48 @@ import se.sveaekonomi.webpay.integration.adminservice.UpdateOrderRowsRequest;
 import se.sveaekonomi.webpay.integration.config.ConfigurationProvider;
 import se.sveaekonomi.webpay.integration.order.OrderBuilder;
 import se.sveaekonomi.webpay.integration.order.row.NumberedOrderRowBuilder;
-import se.sveaekonomi.webpay.integration.util.constant.COUNTRYCODE;
 import se.sveaekonomi.webpay.integration.util.constant.PAYMENTTYPE;
 
 /**
  * @author Kristian Grossman-Madsen
  */
-
 public class UpdateOrderRowsBuilder extends OrderBuilder<UpdateOrderRowsBuilder> {
 	
-    private ConfigurationProvider config;
-    private COUNTRYCODE countryCode;
-
-	private ArrayList<NumberedOrderRowBuilder> updateOrderRows;
-    private Long orderId;
 	private PAYMENTTYPE orderType;
 
-	public UpdateOrderRowsBuilder( ConfigurationProvider config ) {
-		this.config = config;
-		this.updateOrderRows = new ArrayList<NumberedOrderRowBuilder>();
-	}
+	private Long orderId;
+	private ArrayList<NumberedOrderRowBuilder> updateOrderRows;
 
-	public ConfigurationProvider getConfig() {
-		return config;
-	}
-
-	public UpdateOrderRowsBuilder setConfig(ConfigurationProvider config) {
-		this.config = config;
-		return this;
-	}
-
-	public COUNTRYCODE getCountryCode() {
-		return countryCode;
-	}
-
-	public UpdateOrderRowsBuilder setCountryCode(COUNTRYCODE countryCode) {
-		this.countryCode = countryCode;
-		return this;
-	}
+	
+	public PAYMENTTYPE getOrderType() {
+		return this.orderType;
+	}	
 
 	public Long getOrderId() {
 		return orderId;
 	}
-
 	public UpdateOrderRowsBuilder setOrderId(Long orderId) {
 		this.orderId = orderId;
 		return this;
-	}		
-	
+	}
+	public ArrayList<NumberedOrderRowBuilder> getUpdateOrderRows() {
+		return updateOrderRows;
+	}
 	public UpdateOrderRowsBuilder addUpdateOrderRow(NumberedOrderRowBuilder updateOrderRow) {
 		this.updateOrderRows.add(updateOrderRow);
 		return this;
 	}	
-
 	public UpdateOrderRowsBuilder addUpdateOrderRows(ArrayList<NumberedOrderRowBuilder> updateOrderRows) {
 		this.updateOrderRows.addAll(updateOrderRows);
 		return this;
 	}
-
-	public ArrayList<NumberedOrderRowBuilder> getUpdateOrderRows() {
-		return updateOrderRows;
+	
+	
+	public UpdateOrderRowsBuilder( ConfigurationProvider config ) {
+		this.config = config;
+		this.updateOrderRows = new ArrayList<NumberedOrderRowBuilder>();
 	}
-
+	
 	public UpdateOrderRowsRequest updateInvoiceOrderRows() {
     	this.orderType = PAYMENTTYPE.INVOICE;
 		return new UpdateOrderRowsRequest(this);
@@ -77,9 +57,4 @@ public class UpdateOrderRowsBuilder extends OrderBuilder<UpdateOrderRowsBuilder>
     	this.orderType = PAYMENTTYPE.PAYMENTPLAN;
 		return new UpdateOrderRowsRequest(this);
 	}
-
-	public PAYMENTTYPE getOrderType() {
-		return this.orderType;
-	}
-	
 }
